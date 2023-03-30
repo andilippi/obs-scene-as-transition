06a3745a7e64a817d58607aaa2f41801b28aff58
andilippi <64610623+andilippi@users.noreply.github.com>
2023-03-30 13:07:44 +0100

    v1.0.0 - prerelease
    Added Set Total Transition Duration
    
    Added Set Transition Point (Time or Percentage)
    
    Added option to choose a filter to trigger on the transition scene when the transition starts

--- scene-as-transition.c
+++ scene-as-transition.c
@@ -4,9 +4,11 @@
 struct scene_as_transition {
 	obs_source_t *source;
 	obs_source_t *transition_scene;
+	obs_source_t *filter;
 	bool transitioning;
 	float transition_point;
 	float duration;
+	const char *filter_name;
 };
 
 static const char *scene_as_transition_get_name(void *type_data)
@@ -22,6 +24,7 @@ void scene_as_transition_update(void *data, obs_data_t *settings)
 	obs_source_release(st->transition_scene);
 	st->transition_scene =
 		obs_get_source_by_name(obs_data_get_string(settings, "scene"));
+
 
 	st->duration = (float)obs_data_get_double(settings, "duration");
 	obs_transition_enable_fixed(st->source, true, (uint32_t)st->duration);
@@ -39,7 +42,16 @@ void scene_as_transition_update(void *data, obs_data_t *settings)
 					       settings, "transition_point") /
 				       100.0f;
 	}
+
+	const char *filter_name = obs_data_get_string(settings, "filter");
+
+	if (st->filter)
+		obs_source_release(st->filter);
+
+	st->filter = obs_source_get_filter_by_name(st->transition_scene,
+						   filter_name);
 }
+
 
 static void *scene_as_transition_create(obs_data_t *settings,
 					obs_source_t *source)
@@ -82,6 +94,8 @@ static void scene_as_transition_video_render(void *data, gs_effect_t *effect)
 				obs_source_inc_showing(st->transition_scene);
 			if (obs_source_active(st->source))
 				obs_source_inc_active(st->transition_scene);
+			if (st->filter)
+				obs_source_set_enabled(st->filter, true);
 		}
 		obs_source_video_render(st->transition_scene);
 	} else if (st->transitioning) {
@@ -127,20 +141,12 @@ static enum gs_color_space scene_as_transition_video_get_color_space(
 	return obs_transition_video_get_color_space(st->source);
 }
 
-bool scene_transition_list_add_scene(void *data, obs_source_t *transition_scene)
-{
-	obs_property_t *prop = data;
-	obs_property_list_add_string(prop,
-				     obs_source_get_name(transition_scene),
-				     obs_source_get_name(transition_scene));
-	return true;
-}
-
 void scene_as_transition_defaults(obs_data_t *settings)
 {
 	obs_data_set_default_double(settings, "duration", 1000.0);
 	obs_data_set_default_double(settings, "transition_point", 50.0);
 	obs_data_set_default_double(settings, "transition_point_ms", 500.0);
+	obs_data_set_default_string(settings, "filter", "No Filter Selected");
 }
 
 static bool transition_point_type_modified(obs_properties_t *ppts,
@@ -165,19 +171,70 @@ static bool transition_point_type_modified(obs_properties_t *ppts,
 	return true;
 }
 
+bool scene_as_transition_list_add_scene(void *data,
+					obs_source_t *transition_scene)
+{
+	obs_property_t *prop = data;
+	const char *name = obs_source_get_name(transition_scene);
+	obs_property_list_add_string(prop, name, name);
+	return true;
+}
+
+bool scene_as_transition_list_add_filter(obs_source_t *parent,
+					 obs_source_t *child, void *data)
+{
+	UNUSED_PARAMETER(parent);
+	obs_property_t *p = data;
+	const char *name = obs_source_get_name(child);
+	obs_property_list_add_string(p, name, name);
+	return true;
+}
+
+static bool scene_modified(obs_properties_t *props, obs_property_t *property,
+			   obs_data_t *settings)
+{
+	obs_property_t *filter = obs_properties_get(props, "filter");
+	const char *scene_name = obs_data_get_string(settings, "scene");
+	obs_source_t *scene = obs_get_source_by_name(scene_name);
+
+	obs_property_list_clear(filter);
+	obs_property_list_add_string(
+		filter, obs_module_text("No Filter Selected"), "filter");
+	obs_source_enum_filters(scene, scene_as_transition_list_add_filter,
+				filter);
+
+	obs_data_set_string(settings, "filter", "No Filter Selected");
+
+	obs_source_release(scene);
+
+	return true;
+}
+
 obs_properties_t *scene_as_transition_properties(void *data)
 {
+
+	struct scene_as_transition *st = data;
+
 	obs_properties_t *props = obs_properties_create();
 
 	obs_property_t *scene = obs_properties_add_list(
 		props, "scene", "Scene", OBS_COMBO_TYPE_EDITABLE,
 		OBS_COMBO_FORMAT_STRING);
-	obs_enum_scenes(scene_transition_list_add_scene, scene);
+	obs_property_set_long_description(
+		scene,
+		obs_module_text(
+			"Select the scene you wish to use as the transition."));
+	obs_enum_scenes(scene_as_transition_list_add_scene, scene);
+	obs_property_set_modified_callback(scene, scene_modified);
 
 	obs_property_t *p = obs_properties_add_float(
-		props, "duration", obs_module_text("Total Duration"), 0.0,
-		30000.0, 100.0);
+		props, "duration", obs_module_text("Duration"), 0.0, 30000.0,
+		100.0);
 	obs_property_float_set_suffix(p, " ms");
+	obs_property_set_long_description(
+		p,
+		obs_module_text(
+			"The total duration of the transition in milliseconds."));
 
 	obs_properties_t *transition_point_group = obs_properties_create();
 
@@ -190,18 +247,42 @@ obs_properties_t *scene_as_transition_properties(void *data)
 				    OBS_COMBO_TYPE_LIST, OBS_COMBO_FORMAT_INT);
 	obs_property_list_add_int(p, obs_module_text("Percentage"), 0);
 	obs_property_list_add_int(p, obs_module_text("Time"), 1);
+	obs_property_set_long_description(
+		p,
+		obs_module_text(
+			"Select the type of transition point (percentage or time)."));
 	obs_property_set_modified_callback(p, transition_point_type_modified);
 	p = obs_properties_add_float_slider(transition_point_group,
 					    "transition_point",
 					    obs_module_text("Transition Point"),
 					    0, 100.0, 1.0);
 	obs_property_float_set_suffix(p, "%");
+	obs_property_set_long_description(
+		p,
+		obs_module_text(
+			"The position of the transition point as a percentage of the total duration."));
 
 	p = obs_properties_add_float(transition_point_group,
 				     "transition_point_ms",
 				     obs_module_text("Transition Point"), 0,
 				     30000.0, 100.0);
 	obs_property_float_set_suffix(p, " ms");
+	obs_property_set_long_description(
+		p,
+		obs_module_text(
+			"The position of the transition point in milliseconds."));
+
+	obs_property_t *filter = obs_properties_add_list(
+		props, "filter", obs_module_text("Filter To Trigger"),
+		OBS_COMBO_TYPE_EDITABLE, OBS_COMBO_FORMAT_STRING);
+	obs_property_list_add_string(
+		filter, obs_module_text("No Filter Selected"), "filter");
+	obs_property_set_long_description(
+		filter,
+		obs_module_text(
+			"Selecting a filter here will enable it when the transition begins."));
+	obs_source_enum_filters(st->transition_scene,
+				scene_as_transition_list_add_filter, filter);
 
 	obs_properties_add_text(
 		props, "plugin_info",
